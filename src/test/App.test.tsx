607708968e37780ebefe418757e173a0fc884ca2
bryce-bardwell <bryce.bardwell@sky.uk>
2023-11-03 15:31:10 +0000

    Refactor tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -10,37 +10,41 @@ const getBackgroundColour = async (page: Page, selector: string) => {
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test('Page title is correct', async ({ mount }) => {
-  const app = await mount(<App />);
-  await expect(app).toContainText('Insanely Creative React App');
+test.describe('Page Content', async () => {
+  test('should have correct page title', async ({ mount }) => {
+    const app = await mount(<App />);
+    await expect(app).toContainText('Insanely Creative React App');
+  });
+  
+  test('should have correct button description', async ({ mount }) => {
+    const app = await mount(<App />);
+    const description = app.locator('.description');
+    await expect(description).toContainText('Click the button to change the background colour.')
+  });
 });
 
-test('Button description is correct', async ({ mount }) => {
-  const app = await mount(<App />);
-  const description = app.locator('.description');
-  await expect(description).toContainText('Click the button to change the background colour.')
-})
-
-test('Clicking the button changes the background colour', async({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.waitForSelector('.bg-button');
-
-  const before = await getBackgroundColour(page, 'body');
-  await page.click('.bg-button');
-  const after = await getBackgroundColour(page, 'body');
-
-  expect(before).not.toEqual(after);
-});
-
-test('Clicking the button multiple times changes the background colour each time', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.waitForSelector('.bg-button');
-
-  for (let i = 0; i < 10; i++) {
+test.describe('Button Functionality', async() => {
+  test('should change background colour when button clicked once', async({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('.bg-button');
+  
     const before = await getBackgroundColour(page, 'body');
     await page.click('.bg-button');
     const after = await getBackgroundColour(page, 'body');
   
     expect(before).not.toEqual(after);
-  }
-})
+  });
+  
+  test('should change background colour each time button is clicked', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('.bg-button');
+  
+    for (let i = 0; i < 10; i++) {
+      const before = await getBackgroundColour(page, 'body');
+      await page.click('.bg-button');
+      const after = await getBackgroundColour(page, 'body');
+    
+      expect(before).not.toEqual(after);
+    }
+  });
+});
